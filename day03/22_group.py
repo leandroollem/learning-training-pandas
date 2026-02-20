f46127d6caeb83605f291ad6c65133efcdefe20c
Lendro <leandromello2001@gmail.com>
2026-02-20 14:41:05 -0300

    feat: Group by and Merge

---
+++ day03/22_group.py
@@ -0,0 +1,36 @@
+# %%
+import pandas as pd
+transacoes = pd.read_csv("../data/transacoes.csv", sep=";")
+transacoes.head()
+
+# %%
+# Aplicando count em cada coluna para cada cliente (como índice)
+# Retorna um DataFrame
+transacoes.groupby(by=["IdCliente"]).count()
+
+# %%
+# Retorna uma Serie
+transacoes.groupby(by=["IdCliente"])["IdTransacao"].count()
+
+# %%
+# Retorna um DataFrame
+transacoes.groupby(by=["IdCliente"])[["IdTransacao"]].count()
+
+# %%
+# Caso queira que retorna o IdCliente como coluna e não index
+transacoes.groupby(by=["IdCliente"], as_index=False)[["IdTransacao"]].count()
+
+# %%
+# Somar quantidade de transações, quantidade de pontos e a média de pontos por transação para cada cliente
+summary = (transacoes.groupby(by=["IdCliente"], as_index=False)
+                     .agg({
+                            "IdTransacao":["count"],
+                            "QtdePontos":["sum", "mean"]
+                        }))
+
+# %%
+summary[("QtdePontos", "mean")]
+# %%
+summary.columns = ["IdCliente", "qtdeTransacao", "totalPontos", "avgPontos"]
+summary
+# %%
