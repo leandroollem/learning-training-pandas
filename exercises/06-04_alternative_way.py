b17b442ec24cde9ee66ad9c123892e6c6e0e463a
Lendro <leandromello2001@gmail.com>
2026-02-20 14:41:56 -0300

    feat: Group by and Merge

--- exercises/06-04_alternative_way.py
+++ exercises/06-04_alternative_way.py
@@ -2,21 +2,16 @@
 # %%
 import pandas as pd
 transacoes = pd.read_csv("../data/transacoes.csv", sep=";")
-transacoes.head()
-# %%
 transacao_produto = pd.read_csv("../data/transacao_produto.csv", sep=";")
-transacao_produto.head()
-# %%
 produtos = pd.read_csv("../data/produtos.csv", sep=";")
-produtos.head()
 
-# %%
 produtos = produtos[produtos["DescNomeProduto"]=="Presença Streak"]
 
 (transacoes.merge(transacao_produto, on="IdTransacao",how="left")
-           .merge(produtos, on=["IdProduto"], how="right")
-           .groupby(by=["IdCliente"])["IdTransacao"]
+           .merge(produtos, on="IdProduto", how="right")
+           .groupby(by="IdCliente")["IdTransacao"]
            .count()
            .sort_values(ascending=False)
            .head(1)
 )
+# Da pra fazer com INNER JOIN no segundo merge
